6a711a29853a2a842859f009143f97c3de1aa4fb
JamesDoane <ijamesdoaneok@gmail.com>
2022-01-31 09:46:41 -0600

    Project skeleton setup.

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        this is the page so far
+      </div>
+    )
+  }
+}
+
+export default App;
